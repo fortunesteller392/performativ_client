f90c2feb6446ee664612901338fcd65420d74a24
Diamond <fortunes.teller@outlook.com>
2023-11-13 16:33:29 +0900

    Add constants

---
+++ src/consts/index.ts
@@ -0,0 +1,3 @@
+export const API_URL: string = "http://localhost:8000/api";
+export const WIKI_URL: string =
+  "https://en.wikipedia.org/api/rest_v1/page/summary";
---
+++ src/consts/route.ts
@@ -0,0 +1,7 @@
+const USERS: string = "/users";
+const USER_CREATE: string = `${USERS}/create`;
+const USER_UPDATE: string = `${USERS}/update`;
+
+const WIKI: string = "/wiki";
+
+export const PATH = { USERS, USER_CREATE, USER_UPDATE, WIKI };
